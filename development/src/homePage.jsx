79e42c83812b3f334a25711e54ab1df7f3c461e0
Katieum <eaglenyoni@gmail.com>
2025-07-14 18:38:37 +0200

    Little experimental edits to homepage

--- development/src/homePage.jsx
+++ development/src/homePage.jsx
@@ -1,12 +1,24 @@
 import {Helmet} from 'react-helmet'
 
+function LandindDisplay(){
+    return(
+    <>
+    <div className='Landing-Display'>
+        <h1>Katleho Nyoni</h1>
+        <p>is not just a name, it's a <b>Brand</b></p>
+        <b>Worth Remembering!</b>
+    </div>
+    </>
+
+    );
+}
 export default function HomePage(){
     return(
         <>
         <Helmet>
             <title>Katleho Nyoni | Lord Eagle(L.E)</title>
         </Helmet>
-        <h1>Welcome Home!</h1>
+        <LandindDisplay />
         </>
     );
 }
