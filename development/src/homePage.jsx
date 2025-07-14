1d32a6d528f9b84854819c4e1b93f2c6ce94497b
Katieum <eaglenyoni@gmail.com>
2025-07-14 20:50:53 +0200

    Included the Lavalamb fluid-blob

--- development/src/homePage.jsx
+++ development/src/homePage.jsx
@@ -1,9 +1,11 @@
 import {Helmet} from 'react-helmet'
+import { LavaLamp } from './fluidBlob';
 
 function LandindDisplay(){
     return(
     <>
     <div className='Landing-Display'>
+        <LavaLamp />
         <h1>Katleho Nyoni</h1>
         <p>is not just a name, it's a <b>Brand</b></p>
         <b>Worth Remembering!</b>
