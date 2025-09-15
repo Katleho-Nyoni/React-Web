e2c604df63801fec0b2813f0fa924cc5ec422f98
Katieum <eaglenyoni@gmail.com>
2025-09-15 21:13:24 +0200

    Editing the Helmet

--- development/src/projectPage.jsx
+++ development/src/projectPage.jsx
@@ -84,7 +84,7 @@ export default function ProjectPage(){
     return(
         <>
         <Helmet>
-            <title>Projects | L.E Nyoni</title>
+            <title>Projects | Katleho Nyoni</title>
         </Helmet>
             {/* <h1>Projects</h1> */}
             <SlinderNews />
