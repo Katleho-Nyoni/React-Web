fd306303651830e4584600e7a478975ca1e496e7
Katieum <eaglenyoni@gmail.com>
2025-10-21 23:18:52 +0200

    Almost there, looking good

--- development/src/projectPage.jsx
+++ development/src/projectPage.jsx
@@ -1,6 +1,6 @@
 import {Helmet} from 'react-helmet'
 import { Link } from "react-router-dom";
-import HeroPicture from './assets/Eagle.webp'
+import City from './assets/pexels-visionmonkeyandy-1034662.jpg'
 import { useState } from 'react';
 
 function SlinderNews(){
@@ -17,7 +17,7 @@ function SlinderNews(){
 }
 
 function TabProjects(){
-    let x = "📖", y = "📊",z = "🛠";
+    let /*x = "📖",*/ y = "📊",z = "🛠";
 
     const [toggle,setToggle] = useState();
     
@@ -29,23 +29,23 @@ return(
     <>
     <div className='tab-section'>
         <ul className='tab-list'>
-            <li className='tab' onClick={()=>updateToggle(1)}>{x} <b>Books</b> </li>
-            <li className='tab' onClick={()=>updateToggle(2)}>{y} <b>Reports</b> </li>
-            <li className='tab' onClick={()=>updateToggle(3)}>{z} <b>Contributions</b> </li>
+            {/* <li className='tab' onClick={()=>updateToggle(1)}>{x} <b>Books</b> </li> */}
+            <li className='tab' onClick={()=>updateToggle(1)}>{y} <b>Reports</b> </li>
+            <li className='tab' onClick={()=>updateToggle(2)}>{z} <b>Contributions</b> </li>
         </ul>
 
-        <div className={toggle===1?'show-content':'content'}>
+        {/* <div className={toggle===1?'show-content':'content'}>
             <p>This section will include the books I have written alone or concurrently with other authors.</p>
-        </div>
-        <div className={toggle===2?'show-content':'content'}>
-            <p>Below are the reports I have written</p>
+        </div> */}
+        <div className={toggle===1?'show-content':'content'}>
+            <p>Below are the reports I have written:</p>
             <span className='articles'>
                 <p>1. Will I Eventually be Employed? A Mathematical Approach</p>
                 <p>2. Forecasting the sales of Food & Beverages of RSA</p>
                 <p>3. Employee's Satisfaction</p>
             </span>
         </div>
-        <div className={toggle===3?'show-content':'content'}>
+        <div className={toggle===2?'show-content':'content'}>
            <p> I haven't contributed to any open-source project yet.</p>
         </div>
     </div>
@@ -59,11 +59,10 @@ function MainProjects(){
         <div className='main-project'>
             <div className='card-container'>
             <h3>Data Group Consulting</h3>
-            <h5>Fullstack</h5>
+            <h5>FullStack</h5>
             <p>This is a team of AI Agents that take your dataset(csv) and 
                 generate a pdf Report.</p>
             <button>Learn More</button>
-            <Link to='/a/login'><button>Use Demo</button></Link>
             </div>
         </div>
         </>
@@ -74,16 +73,45 @@ function CurrentProject(){
     return(
         <>
         <div className='current-project'>
-            {/* <img src={HeroPicture} alt=""/> */}
-            <h1>Currently Working on</h1>
-            <div className='card-container'>
-            <h3>Data Group Consulting</h3>
-            <h5>Fullstack</h5>
-            <p>This is a team of AI Agents that take your dataset(csv) and 
-                generate a pdf Report.</p>
-            <button>Learn More</button>
-            <button><Link to='/a/login'>Use Demo</Link></button>
-            </div>
+            <img src={City} alt=""/>
+            <span className='card-area'>
+                <div className='card-container'>
+                    <h3>Katieum</h3>
+                    <h5>Educational</h5>
+                    <p>Platform to teach grade 10 to 12 Mathematics using animation. Physical Science to join later.</p>
+                    <button>Learn More</button>
+                </div>
+                <div className='card-container'>
+                    <h3>Katieum's Web App</h3>
+                    <h5>Client Only</h5>
+                    <p>Web app that would provide learners with math lessons & other resources without having to login.</p>
+                    <button>Visit Site</button>
+                </div>
+                <div className='card-container'>
+                    <h3>Learning Angular</h3>
+                    <h5>FullStack</h5>
+                    <p>Learning Angular.JS through a tutorial building a Buy & Sell store.</p>
+                    <button>Learn More</button>
+                </div>
+                <div className='card-container'>
+                    <h3>Learning Next.JS</h3>
+                    <h5>FullStack</h5>
+                    <p>Learning Next.JS through a tutorial building an e-commerce store.</p>
+                    <button>Learn More</button>
+                </div>
+                <div className='card-container'>
+                    <h3>Data Group Consulting</h3>
+                    <h5>FullStack</h5>
+                    <p>This is a team of AI Agents that take your dataset(csv) and generate a pdf Report.</p>
+                    <button>Learn More</button>
+                </div>
+                <div className='card-container'>
+                    <h3>My/This Website</h3>
+                    <h5>Client Only</h5>
+                    <p>This is a personal website built to showcase my projects and skills.</p>
+                    <button>Learn More</button>
+                </div>
+            </span>
         </div>
         </>
     );
@@ -92,17 +120,14 @@ function CurrentProject(){
 export default function ProjectPage(){
     return(
         <>
-        <Helmet>
-            <title>Projects | Katleho Nyoni</title>
-        </Helmet>
-            {/* <h1>Projects</h1> */}
-            {/* <SlinderNews /> */}
+            <Helmet>
+                <title>Projects | Katleho Nyoni</title>
+            </Helmet>
+            <CurrentProject />
             <div className='project-tabs'>
                 <TabProjects />
             </div>
-            {/* <MainProjects /> */}
-            <CurrentProject />
-            {/* <FooterSection /> */}
+            
         </>
     );
 }
