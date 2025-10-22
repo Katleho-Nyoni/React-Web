708cc34433a7a5dd343b213d8784917ef887ce34
Katieum <eaglenyoni@gmail.com>
2025-10-22 11:13:16 +0200

    Fixed the child link for home

--- development/src/App.jsx
+++ development/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Layout from './layout'
 import HomePage from './homePage'
 import AboutPage from './aboutPage'
-import ContactPage from './contactPage'
 import ProjectPage from './projectPage'
 import KatieumPage from './katieumPage'
 import ProductsPage from './prodPage'
@@ -11,23 +10,13 @@ import ProductsPage from './prodPage'
 const routes = [
   { path: '/', element: <Layout />,
   children: [
-  {path: '/', element: <HomePage />},
+  {index: true, element: <HomePage />},
   {path: '/about', element: <AboutPage />},
-  // {path: '/contact', element: <ContactPage />},
   {path: '/projects', element: <ProjectPage />},
   {path: '/katieum', element: <KatieumPage />},
   {path: '/products', element: <ProductsPage />},
   ],},
 ];
-
-// const routes = [
-//   {path: '/', element: <HomePage />},
-//   {path: '/about', element: <AboutPage />},
-//   {path: '/contact', element: <ContactPage />},
-//   {path: '/projects', element: <ProjectPage />},
-// ];
-
-
 
 const router = createBrowserRouter(routes);
 
